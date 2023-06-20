ecbde48ffd6f281b8146b2e38894103c69c63238
Mohamad <no.future2e1@gmail.com>
2023-06-20 15:15:39 +0300

    add material to box

--- src/Box.tsx
+++ src/Box.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import React, { useRef, useState } from 'react';
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
 import { Euler, StringKeyframeTrack } from 'three';
+import { useLoader } from "@react-three/fiber";
 
 export interface BoxProps {
   position: number[];
@@ -11,10 +12,12 @@ export interface BoxProps {
   size: number[];
   meshRef?: React.Ref<THREE.Mesh>;
   color?: string;
+  material?: THREE.Material;
 }
 
+
 export function Box(
-  { position, size, rot, color, meshRef }: BoxProps,
+  { position, size, rot, color, meshRef, material }: BoxProps,
   props: ThreeElements['mesh'],
 ) {
   const ref = useRef<THREE.Mesh>(null!);
@@ -26,9 +29,9 @@ export function Box(
       position={[position[0], position[1], position[2]]}
       rotation={rot}
       ref={meshRef || ref}
+      material={material}
     >
       <boxGeometry args={[size[0], size[1], size[2]]} />
-      <meshStandardMaterial color={color || 'blue'} />
     </mesh>
   );
 }
